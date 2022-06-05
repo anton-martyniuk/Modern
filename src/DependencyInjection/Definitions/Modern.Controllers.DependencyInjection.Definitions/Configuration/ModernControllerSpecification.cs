fbce050625119c388152520ca7620fde554d2dc1
anton-martyniuk <anton.martynyuk@gmail.com>
2022-06-05 18:04:42 +0300

    [+] Controllers DI

---
+++ src/DependencyInjection/Definitions/Modern.Controllers.DependencyInjection.Definitions/Configuration/ModernControllerSpecification.cs
@@ -0,0 +1,22 @@
+﻿namespace Modern.Controllers.DependencyInjection.Definitions.Configuration;
+
+/// <summary>
+/// The modern controller specification model
+/// </summary>
+public class ModernControllerSpecification
+{
+    /// <summary>
+    /// The type of entity returned from the controller
+    /// </summary>
+    public Type EntityDtoType { get; set; } = default!;
+
+    /// <summary>
+    /// The type of entity contained in the data store
+    /// </summary>
+    public Type EntityDboType { get; set; } = default!;
+
+    /// <summary>
+    /// The type of entity identifier
+    /// </summary>
+    public Type EntityIdType { get; set; } = default!;
+}
---
+++ src/DependencyInjection/Modern.Controllers.DataStore.DependencyInjection/Configuration/ModernControllersOptions.cs
@@ -0,0 +1,58 @@
+﻿using Modern.Controllers.DependencyInjection.Definitions.Configuration;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Represents a modern controllers options for registering in DI
+/// </summary>
+public class ModernControllersOptions
+{
+    /// <summary>
+    /// Collection of modern controllers specifications
+    /// </summary>
+    internal List<ModernControllerSpecification> Controllers { get; } = new();
+
+    /// <summary>
+    /// Collection of modern controllers specifications
+    /// </summary>
+    internal List<ModernControllerConcreteSpecification> ConcreteControllers { get; } = new();
+
+    /// <summary>
+    /// Adds controller
+    /// </summary>
+    /// <typeparam name="TEntityDto">The type of entity returned from the controller</typeparam>
+    /// <typeparam name="TEntityDbo">The type of entity contained in the data store</typeparam>
+    /// <typeparam name="TId">The type of entity identifier</typeparam>
+    public void AddController<TEntityDto, TEntityDbo, TId>()
+        where TEntityDto : class
+        where TEntityDbo : class
+        where TId : IEquatable<TId>
+    {
+        var configuration = new ModernControllerSpecification
+        {
+            EntityDtoType = typeof(TEntityDto),
+            EntityDboType = typeof(TEntityDbo),
+            EntityIdType = typeof(TId)
+        };
+
+        Controllers.Add(configuration);
+    }
+
+    /// <summary>
+    /// Adds controller
+    /// </summary>
+    /// <typeparam name="TServiceImplementation">The type of concrete controller implementation</typeparam>
+    public void AddController<TServiceImplementation>()
+        where TServiceImplementation : class
+    {
+        // TODO: check if is assignable from types
+
+        var configuration = new ModernControllerConcreteSpecification
+        {
+            ImplementationType = typeof(TServiceImplementation)
+        };
+
+        ConcreteControllers.Add(configuration);
+    }
+}
