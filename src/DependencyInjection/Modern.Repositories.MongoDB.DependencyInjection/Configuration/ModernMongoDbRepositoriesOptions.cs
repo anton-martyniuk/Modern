80b412cd36a5120a5de74aabcfd08c1cb100a275
anton.martyniuk <anton.martynyuk@gmail.com>
2022-08-19 11:30:12 +0300

    [+] Modern.Repositories.MongoDB.DependencyInjection.csproj

---
+++ src/DependencyInjection/Modern.Repositories.MongoDB.DependencyInjection/Configuration/ModernMongoDbRepositoriesOptions.cs
@@ -0,0 +1,62 @@
+﻿using Modern.Repositories.MongoDB.DependencyInjection.Configuration;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Represents a modern repository options for registering in DI
+/// </summary>
+public class ModernMongoDbRepositoriesOptions
+{
+    /// <summary>
+    /// Collection of modern repository specifications
+    /// </summary>
+    public List<ModernMongoDbRepositorySpecification> Repositories { get; } = new();
+
+    /// <summary>
+    /// Collection of modern repository specifications
+    /// </summary>
+    public List<ModernMongoDbRepositoryConcreteSpecification> ConcreteRepositories { get; } = new();
+
+    /// <summary>
+    /// Adds repository
+    /// </summary>
+    /// <param name="lifetime">Repository lifetime in DI</param>
+    /// <typeparam name="TEntity">The type of entity</typeparam>
+    /// <typeparam name="TId">The type of entity identifier</typeparam>
+    public void AddRepository<TEntity, TId>(ServiceLifetime lifetime = ServiceLifetime.Transient)
+        where TEntity : class
+        where TId : IEquatable<TId>
+    {
+        var configuration = new ModernMongoDbRepositorySpecification
+        {
+            EntityType = typeof(TEntity),
+            EntityIdType = typeof(TId),
+            Lifetime = lifetime
+        };
+
+        Repositories.Add(configuration);
+    }
+
+    /// <summary>
+    /// Adds repository
+    /// </summary>
+    /// <param name="lifetime">Repository lifetime in DI</param>
+    /// <typeparam name="TRepositoryInterface">The type of concrete repository interface</typeparam>
+    /// <typeparam name="TRepositoryImplementation">The type of concrete repository implementation</typeparam>
+    public void AddConcreteRepository<TRepositoryInterface, TRepositoryImplementation>(ServiceLifetime lifetime = ServiceLifetime.Transient)
+        where TRepositoryInterface : class
+        where TRepositoryImplementation : class
+    {
+        // TODO: check if IModernService is assignable from types
+
+        var configuration = new ModernMongoDbRepositoryConcreteSpecification
+        {
+            InterfaceType = typeof(TRepositoryInterface),
+            ImplementationType = typeof(TRepositoryImplementation),
+            Lifetime = lifetime
+        };
+
+        ConcreteRepositories.Add(configuration);
+    }
+}
---
+++ src/DependencyInjection/Modern.Repositories.MongoDB.DependencyInjection/ServicesExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection.Extensions;
+using Modern.Extensions.Microsoft.DependencyInjection.Models;
+using Modern.Repositories.Abstractions;
+using Modern.Repositories.MongoDB;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Modern.Repositories.MongoDB extensions for Microsoft.Extensions.DependencyInjection
+/// </summary>
+public static class ServicesExtensions
+{
+    /// <summary>
+    /// Adds MongoDb repositories into DI
+    /// </summary>
+    /// <param name="builder">Modern services builder</param>
+    /// <param name="configure">Repositories configure delegate</param>
+    /// <returns>IServiceCollection</returns>
+    public static ModernServicesBuilder AddRepositoriesMongoDb(this ModernServicesBuilder builder, Action<ModernMongoDbRepositoriesOptions> configure)
+    {
+        var options = new ModernMongoDbRepositoriesOptions();
+        configure(options);
+
+        foreach (var c in options.Repositories)
+        {
+            var interfaceType = typeof(IModernRepository<,>).MakeGenericType(c.EntityType, c.EntityIdType);
+            var implementationType = typeof(ModernMongoDbRepository<,>).MakeGenericType(c.EntityType, c.EntityIdType);
+
+            builder.Services.TryAdd(new ServiceDescriptor(interfaceType, implementationType, c.Lifetime));
+        }
+
+        foreach (var c in options.ConcreteRepositories)
+        {
+            builder.Services.TryAdd(new ServiceDescriptor(c.InterfaceType, c.ImplementationType, c.Lifetime));
+        }
+
+        return builder;
+    }
+}
