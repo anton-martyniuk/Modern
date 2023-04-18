dfa08a1c1acd12f36b39bdf44ba7cfc1fca6ab2b
anton.martyniuk <anton.martynyuk@gmail.com>
2023-04-18 22:23:52 +0300

    [*] Modern.Controllers.DataStore.DependencyInjection without definitions

--- src/DependencyInjection/Modern.Controllers.DataStore.DependencyInjection/Configuration/ModernControllersOptions.cs
+++ src/DependencyInjection/Modern.Controllers.DataStore.DependencyInjection/Configuration/ModernControllersOptions.cs
@@ -1,4 +1,4 @@
-﻿using Modern.Controllers.DependencyInjection.Definitions.Configuration;
+﻿using Modern.Controllers.DataStore.DependencyInjection.Configuration;
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection;
