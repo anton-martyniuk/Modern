02782569039366acc3d66c77b457a6e072246460
anton.martyniuk <anton.martynyuk@gmail.com>
2023-05-27 19:36:53 +0300

    [*] Modern.sln

---
+++ src/DependencyInjection/Modern.GraphQL.HotChocolate.DataStore.DependencyInjection/Configuration/ModernGraphQlSpecification.cs
@@ -0,0 +1,22 @@
+﻿namespace Modern.GraphQL.HotChocolate.DataStore.DependencyInjection.Configuration;
+
+/// <summary>
+/// The modern GraphQL specification model
+/// </summary>
+public class ModernGraphQlSpecification
+{
+    /// <summary>
+    /// The type of entity returned from the GraphQL Query or Mutation
+    /// </summary>
+    public Type EntityDtoType { get; set; } = default!;
+
+    /// <summary>
+    /// The type of entity contained in the data store
+    /// </summary>
+    public Type EntityDboType { get; set; } = default!;
+
+    /// <summary>
+    /// The type of entity identifier
+    /// </summary>
+    public Type EntityIdType { get; set; } = default!;
+}
